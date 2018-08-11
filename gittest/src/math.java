0a19071f9c903d7cfcbff5a8e90da5e7bf65d471
azhilenko <azhilenko@gmail.com>
2018-08-11 16:41:09 +0300

    В math добавил массивы, в operators поиск и проверку условий через операторы

---
+++ gittest/src/math.java
@@ -0,0 +1,32 @@
+public class math {
+    public static void main(String[] args) {
+
+//        float num1=23.67f, num2=42.4f, res;                                               //Оператор сложения
+//        res=num1 + num2;
+//        System.out.println(res);
+
+//              Массивы
+        //Одномерный
+//        int [] arr=new int[]{56,489,325};
+////        arr[0]=23;            //Присваивание значения элементу массива
+////        arr[2]=657;
+//        for (int i=0; i<3; i++)         //Вывод элементов массива
+//            System.out.println(i+" element is "+arr[i]);
+        //Многомерныйй
+//        int [][] arr = new int[][]{
+//                {23,78,65,78},
+//                {23,78,65,78},
+//                {23,78,65,78}
+//        };
+//
+//        arr[1][1] = 34;
+//        for (int i=0; i<3; i++)
+//        {
+//            for (int y=0; y<4; y++) {
+//                System.out.print(arr[i][y] + " ");
+//            }
+//            System.out.println("");
+//        }
+
+    }
+}
---
+++ gittest/src/operators.java
@@ -0,0 +1,53 @@
+import java.sql.SQLOutput;
+
+public class operators {
+    public static void main(String[] args) {
+//                                                                          //Проверка на условие
+//        int i=224, j=45;
+//        if (i==225 || j==45){
+//            System.out.println("i is not 225");
+//        } else if (i>155 ) {
+//            System.out.println("i is bigger than 155");
+//        } else {
+//            System.out.println("i is unknown");
+//        }
+
+
+//        int num=15;                                                        //Проверка на условия
+//        switch (num){
+//            case 1:
+//                System.out.println("num is 1");
+//                break;
+//            case 2:
+//                System.out.println("num is 2");
+//                break;
+//            case 15:
+//                System.out.println("num is 15");
+//                break;
+//                default:
+//                    System.out.println("Num is unknown");
+//        }
+
+
+//                                                                          //Поиск числа в переменной через for
+//        int num=15,res=0;
+//        for (int i=0; i<=num; i++ )
+//            res=i;
+//        System.out.println("num is "+res);
+
+
+//        int num=34, res=0, i=0;                                           //Поиск числа в переменной через while
+//        while (i!=num)
+//            i++;
+//        System.out.println(i);
+
+
+//        int i=100;                                                        // Цикл do...while в который не вошли, но выполнил условие 1 раз
+//        do {
+//            System.out.println("Elemet i is "+i);
+//        }while (i<=15);
+
+
+
+    }
+}
