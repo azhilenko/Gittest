4edc455701cdad16108914d8516587badc6c83b5
azhilenko <azhilenko@gmail.com>
2018-08-11 16:38:02 +0300

    Добавил функцию func

--- gittest/src/main.java
+++ gittest/src/main.java
@@ -2,16 +2,27 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        /*Scanner num=new Scanner(System.in);
-        int i=2018;
-        System.out.println("Enter your year of birth: ");
-        i-=num.nextInt();
-        System.out.println("Your age is - "+i);*/
 
-        Scanner str=new Scanner(System.in);
-        String symbols;
-        System.out.println("Enter text in variable");
-        symbols=str.nextLine();
-        System.out.println("My variable is: "+symbols);
+//        Scanner num=new Scanner(System.in);                                           //        Ввод числа
+//        int i=2018;
+//        System.out.println("Enter your year of birth: ");
+//        i-=num.nextInt();
+//        System.out.println("Your age is - "+i);
+
+
+//        Scanner str=new Scanner(System.in);                                           //        Ввод текста
+//        String symbols;
+//        System.out.println("Enter text in variable");
+//        symbols=str.nextLine(); //для каждого типа переменных есть свою функция
+//        System.out.println("My variable is: "+symbols);
+
+        func (23, 74);
+    }
+
+    private static void func (int a, int b)
+    {
+        int res;
+        res=a+b;
+        System.out.println("Results is: "+res);
     }
 }
